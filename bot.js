574e88e95582a83eccd742020eae597ba8171100
Chris McIntosh <chrismcintosh@fico.com>
2024-05-14 13:57:52 -0500

    initial commit

---
+++ bot.js
@@ -0,0 +1,35 @@
+const { ActivityHandler, MessageFactory } = require('botbuilder');
+
+class EchoBot extends ActivityHandler {
+    constructor() {
+        super();
+        // See https://aka.ms/about-bot-activity-message to learn more about the message and other activity types.
+        this.onMessage(async (context, next) => {
+            const replyText = `Echo: ${ context.activity.text }`;
+            await context.sendActivity(MessageFactory.text(replyText, replyText));
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+
+        this.onMembersAdded(async (context, next) => {
+            const membersAdded = context.activity.membersAdded;
+            const welcomeText = 'Hello and welcome!';
+            for (let cnt = 0; cnt < membersAdded.length; ++cnt) {
+                if (membersAdded[cnt].id !== context.activity.recipient.id) {
+                    await context.sendActivity(MessageFactory.text(welcomeText, welcomeText));
+                }
+            }
+            // By calling next() you ensure that the next BotHandler is run.
+            await next();
+        });
+    }
+
+    async proactiveMessage(context) {
+        const teamsChannelId = 'emulator'
+        const activity = MessageFactory.text('This is a test message to a channel')
+        const [reference] = await TeamsInfo.sendMessageToTeamsChannel(context, activity, teamsChannelId, undefined);//process.env.MicrosoftAppId);
+    }
+
+}
+
+module.exports.EchoBot = EchoBot;
